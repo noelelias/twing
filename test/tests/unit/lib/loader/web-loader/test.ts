5d1336ef3bb681d34000fd7e9992665da67af815
noelelias <schenknoel@gmail.com>
2019-11-02 17:49:38 +0100

    now kida works

--- test/tests/unit/lib/loader/web-loader/test.ts
+++ test/tests/unit/lib/loader/web-loader/test.ts
@@ -8,20 +8,20 @@ import { JSDOM } from 'jsdom';
 
 //demo data
 const template = 
-`shouldBeTemplate1[{% include 'template1.twing' %}]
+`shouldBeTemplate1[<strong>{% include 'template1.twing' %}</strong>]<br>
 shouldBeTemplate2[{% include 'template2.twing' %}]
 shouldNotKnowTemplate3[{% include 'template3.twing' %}]
 templateWithSub[{% include 'templateWithSub.twing' %}]
 dom pong => ping[{{ pong|dom('pong.n1') }}]
-dom pong => ping[{{ pong|dom('pongN2') }}]`;
+dom pong => ping[<strong>{{ pong|dom('pongN2') }}</strong>]`;
 
 const expectedTemplate = 
-`shouldBeTemplate1[template1 loaded]
+`shouldBeTemplate1[<strong>template1 loaded</strong>]<br>
 shouldBeTemplate2[template2 loaded]
 shouldNotKnowTemplate3[dont know this template]
 templateWithSub[templateWithSub loaded [sub template loaded]]
 dom pong =&gt; ping[this is pong.n1]
-dom pong =&gt; ping[this is pongN2]`;
+dom pong =&gt; ping[<strong>this is pongN2</strong>]`;
 
 const data = {pong:'ping'};
 const webLoader = (path:string)=>{
@@ -65,5 +65,6 @@ tape('loader web loader', (test) => {
             });
         });
     });
+    test.true(true);
     test.end();
 });
